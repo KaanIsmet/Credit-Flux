96878efe48f54485c5876308fc679d9d698d7212
KaanIsmet <kaanismet7@gmail.com>
2025-01-23 23:57:32 +0300

    implemented Credit Card entity

--- src/main/java/com/KaanIsmetOkul/CreditFlux/entity/CreditCard.java
+++ src/main/java/com/KaanIsmetOkul/CreditFlux/entity/CreditCard.java
@@ -1,4 +1,84 @@
 package com.KaanIsmetOkul.CreditFlux.entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "credit_card")
 public class CreditCard {
+
+    @Id
+    @Column(unique = true)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "issuer")
+    private String issuer;
+
+    @Column(name = "annual_fee")
+    private BigDecimal annualFee;
+
+    @Column(name = "create_at")
+    private LocalDateTime createAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public CreditCard(String name, String issuer, BigDecimal annualFee, LocalDateTime createAt, LocalDateTime updatedAt) {
+        this.name = name;
+        this.issuer = issuer;
+        this.annualFee = annualFee;
+        this.createAt = createAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIssuer() {
+        return issuer;
+    }
+
+    public void setIssuer(String issuer) {
+        this.issuer = issuer;
+    }
+
+    public BigDecimal getAnnualFee() {
+        return annualFee;
+    }
+
+    public void setAnnualFee(BigDecimal annualFee) {
+        this.annualFee = annualFee;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
