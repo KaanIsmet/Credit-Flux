883d074aed8557e4603f19ea6bc0b943de159bf3
KaanIsmet <kaanismet7@gmail.com>
2025-02-03 21:04:22 +0300

    have SecurityConfig functional

--- src/main/java/com/KaanIsmetOkul/CreditFlux/controller/Controller.java
+++ src/main/java/com/KaanIsmetOkul/CreditFlux/controller/Controller.java
@@ -6,6 +6,7 @@ import com.KaanIsmetOkul.CreditFlux.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,9 @@ public class Controller {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return userRepository.findAll();
@@ -26,6 +30,7 @@ public class Controller {
 
     @PostMapping("/users")
     public ResponseEntity<User> saveUser(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         User savedUser = userRepository.save(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
