574c772d799fd956085e36de9de5acccc5280ec7
KaanIsmet <kaanismet7@gmail.com>
2025-02-20 19:58:06 +0300

    implemented cardReward controller and service

--- src/main/java/com/KaanIsmetOkul/CreditFlux/exceptionHandling/CardRewardNotFound.java
+++ src/main/java/com/KaanIsmetOkul/CreditFlux/exceptionHandling/CardRewardNotFound.java
@@ -1,12 +1,11 @@
 package com.KaanIsmetOkul.CreditFlux.exceptionHandling;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class CreditCardNotFound extends RuntimeException {
-    public CreditCardNotFound(String message) {
+public class CardRewardNotFound extends RuntimeException {
+    public CardRewardNotFound(String message) {
         super(message);
     }
 }
